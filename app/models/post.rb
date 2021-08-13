0ee9025c14493280eb21ae2c6ba71d019e07bed4
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-08-13 21:48:43 +0300

    refactor: add assosiations for users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 4, maximum: 100 }
   validates :text, presence: true, length: { minimum: 10, maximum: 700 }
 end
