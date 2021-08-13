90fa9e7300f08f0dfc1f457d0a6172ca8a2fd72c
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-08-13 20:41:16 +0300

    refactor: add validations for user and for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
-
+  validates :title, presence: true, length: { minimum: 4, maximum: 100 }
+  validates :text, presence: true, length: { minimum: 10, maximum: 700 }
 end
